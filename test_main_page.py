555ed913ac880542c22ad7d015d9dc9a9dc10397
Ruslan Araslanov <araslanov.r.v@yandex.ru>
2021-11-28 20:18:05 +0500

    Test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,44 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import pytest
 from selenium.webdriver.common.by import By
 import time
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+@pytest.mark.skip
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+
+
